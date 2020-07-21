623cccfc4ad3842154f83620d23576eb6125bf04
Haroon Rafique <haroon.rafique@protonmail.com>
2020-07-21 16:46:49 +0200

    02: updated submit all script

--- 02_Mini_Verify_Longitudinal_Matching/Submit_All.py
+++ 02_Mini_Verify_Longitudinal_Matching/Submit_All.py
@@ -6,28 +6,16 @@ SC_Scan = True
 master_dir = os.getcwd()
 
 NoSC_locations = []
-NoSC_locations.append('/0_00')
-NoSC_locations.append('/0_01')
-NoSC_locations.append('/0_02')
-NoSC_locations.append('/0_03')
-NoSC_locations.append('/0_04')
-NoSC_locations.append('/0_10')
-NoSC_locations.append('/0_11')
-NoSC_locations.append('/0_12')
-NoSC_locations.append('/0_13')
-NoSC_locations.append('/0_14')
+NoSC_locations.append('/0_70_0')
+NoSC_locations.append('/0_85_0')
 
 SC_locations = []
-SC_locations.append('/1_00')
-SC_locations.append('/1_01')
-SC_locations.append('/1_02')
-SC_locations.append('/1_03')
-SC_locations.append('/1_04')
-SC_locations.append('/1_10')
-SC_locations.append('/1_11')
-SC_locations.append('/1_12')
-SC_locations.append('/1_13')
-SC_locations.append('/1_14')
+SC_locations.append('/1_70_0')
+SC_locations.append('/1_70_1')
+SC_locations.append('/1_70_2')
+SC_locations.append('/1_85_0')
+SC_locations.append('/1_85_1')
+SC_locations.append('/1_85_2')
 
 if NoSC_Scan:
 	for loc in NoSC_locations:
