00c58d24c92f71644606befa91071ebc2416069b
Haroon Rafique <haroon.rafique@protonmail.com>
2020-07-24 16:58:17 +0200

    01, 02: updated for more RF voltages

--- 02_Mini_Verify_Longitudinal_Matching_BLonD/create_new_scan.sh
+++ 02_Mini_Verify_Longitudinal_Matching_BLonD/create_new_scan.sh
@@ -5,18 +5,24 @@ folder_out=1
 # name folder like: SC_Voltage_Case
 # e.g. 1_80_2
 
-sc=0
+#~ sc=0
 #~ cp -r ${folder_in} ${sc}_${vol}_${vol_case}
-cp -r ${folder_in} ${sc}_70_0
-cp -r ${folder_in} ${sc}_85_0
+#~ cp -r ${folder_in} ${sc}_70_0
+#~ cp -r ${folder_in} ${sc}_85_0
 
 sc=1
 vol=70
-cp -r ${folder_in} ${sc}_${vol}_0
-cp -r ${folder_in} ${sc}_${vol}_1
-cp -r ${folder_in} ${sc}_${vol}_2
+#~ cp -r ${folder_in} ${sc}_${vol}_0
+#~ cp -r ${folder_in} ${sc}_${vol}_1
+#~ cp -r ${folder_in} ${sc}_${vol}_2
+cp -r ${folder_in} ${sc}_${vol}_3
+cp -r ${folder_in} ${sc}_${vol}_4
+cp -r ${folder_in} ${sc}_${vol}_5
 
 vol=85
-cp -r ${folder_in} ${sc}_${vol}_0
-cp -r ${folder_in} ${sc}_${vol}_1
-cp -r ${folder_in} ${sc}_${vol}_2
+#~ cp -r ${folder_in} ${sc}_${vol}_0
+#~ cp -r ${folder_in} ${sc}_${vol}_1
+#~ cp -r ${folder_in} ${sc}_${vol}_2
+cp -r ${folder_in} ${sc}_${vol}_3
+cp -r ${folder_in} ${sc}_${vol}_4
+cp -r ${folder_in} ${sc}_${vol}_5
