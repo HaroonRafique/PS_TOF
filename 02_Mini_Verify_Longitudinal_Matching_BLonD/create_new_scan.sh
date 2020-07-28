590e6e8111bb7bd19cea4d83186393c3852d709e
Haroon Rafique <haroon.rafique@protonmail.com>
2020-07-28 15:48:20 +0200

    02: updated

--- 02_Mini_Verify_Longitudinal_Matching_BLonD/create_new_scan.sh
+++ 02_Mini_Verify_Longitudinal_Matching_BLonD/create_new_scan.sh
@@ -18,9 +18,10 @@ vol=70
 #~ cp -r ${folder_in} ${sc}_${vol}_3
 #~ cp -r ${folder_in} ${sc}_${vol}_4
 #~ cp -r ${folder_in} ${sc}_${vol}_5
-cp -r ${folder_in} ${sc}_${vol}_6
-cp -r ${folder_in} ${sc}_${vol}_7
-cp -r ${folder_in} ${sc}_${vol}_8
+#~ cp -r ${folder_in} ${sc}_${vol}_6
+#~ cp -r ${folder_in} ${sc}_${vol}_7
+#~ cp -r ${folder_in} ${sc}_${vol}_8
+cp -r ${folder_in} ${sc}_${vol}_9
 
 vol=85
 #~ cp -r ${folder_in} ${sc}_${vol}_0
@@ -29,6 +30,7 @@ vol=85
 #~ cp -r ${folder_in} ${sc}_${vol}_3
 #~ cp -r ${folder_in} ${sc}_${vol}_4
 #~ cp -r ${folder_in} ${sc}_${vol}_5
-cp -r ${folder_in} ${sc}_${vol}_6
-cp -r ${folder_in} ${sc}_${vol}_7
-cp -r ${folder_in} ${sc}_${vol}_8
+#~ cp -r ${folder_in} ${sc}_${vol}_6
+#~ cp -r ${folder_in} ${sc}_${vol}_7
+#~ cp -r ${folder_in} ${sc}_${vol}_8
+cp -r ${folder_in} ${sc}_${vol}_9
